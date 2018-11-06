0247f7190bb369e0cf465d2b2b7a8e6d1ba9d116
Jakob <jakob.hust@gmail.com>
2018-11-06 14:11:37 +0100

    Added all sound files, sound manager script and ambient sound to the map.

--- MED7 - Stoneage Project/Assets/Scripts/SoundManager.cs
+++ MED7 - Stoneage Project/Assets/Scripts/SoundManager.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+	// Ambient Sounds
+	public AudioClip Ambient_Nature;
+	public AudioClip Ambient_Sea;
+
+	// Other Sounds
+	public AudioClip Fire_place;
+	public AudioClip Breaking_trap;
+	public AudioClip Drop_tool_in_water;
+	public AudioClip Fish_hook_water;
+	public AudioClip Fish_Catch;
+	public AudioClip Paddle;
+	public AudioClip Orca_Surfacing;
 
 
 	void Start () {
@@ -14,4 +26,10 @@ public class SoundManager : MonoBehaviour {
 	void Update () {
 		
 	}
+
+	public void PlaySound() {
+
+		// Currently does nothing
+	}
+
 }
